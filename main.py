7aa93a16e85cc86d5aa9c0be856b83048247624a
Nicolas Porras Falconio <nporrasfalconio@gmail.com>
2023-04-17 15:37:08 -0700

    init setup

---
+++ main.py
@@ -0,0 +1,13 @@
+from flask import Flask, render_template
+from flask_bootstrap import Bootstrap5
+
+app = Flask(__name__)
+bootstrap = Bootstrap5(app)
+
+@app.route('/')
+def home():
+    return render_template('index.html')
+
+@app.route('/list')
+def list():
+    return render_template('list.html')
