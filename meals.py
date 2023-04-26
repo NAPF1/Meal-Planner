0134ca91121c70ab40e6fd3f0125578cbe042d1a
MattPeters13 <mapeters@csumb.edu>
2023-04-26 15:19:36 -0700

    Addition to meals_info dictionary
    Added img_url for each meal to store the image url from api generation.

--- meals.py
+++ meals.py
@@ -2,29 +2,36 @@ meals_info = [
   {
     "name" : "meal1",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal2",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal3",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal4",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal5",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal6",
     "ingredients" : [],
+    "img_url" : ""
   },
   {
     "name" : "meal7",
     "ingredients" : [],
+    "img_url" : ""
   }
 ]
