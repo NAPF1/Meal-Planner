f891044658d9d10a4bdc27121b00eac52ddbc9e3
NAPF1 <92123409+NAPF1@users.noreply.github.com>
2023-05-02 19:33:03 -0700

    Added some meals with img urls for now.

--- meals.py
+++ meals.py
@@ -1,37 +1,37 @@
 meals_info = [
   {
-    "name" : "meal1",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Bolognese Spaghetti Squash Pasta with Bread",
+    "ingredients" : ['Spaghetti Squash', 'Bolognese Sauce', 'Spicy Sausage', 'Homemade Bread'],
+    "img_url" : "https://everydaydishes.com/wp-content/uploads/2022/08/macro-friendly-dinner-recipes-spaghetti-squash--scaled.jpg"
   },
   {
-    "name" : "meal2",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Stuffed Bell Peppers",
+    "ingredients" : ['Bell Peppers', 'Ground Beef', 'Rice', 'Cheese'],
+    "img_url" : "https://simple-veganista.com/wp-content/uploads/2019/10/best-vegan-stuffed-peppers-recipe-14.jpg"
   },
   {
-    "name" : "meal3",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Chicken with Rice and Mixed Vegetables",
+    "ingredients" : ['Chicken', 'Rice', 'Mixed Veggies', 'Bachan Sauce'],
+    "img_url" : "https://kristineskitchenblog.com/wp-content/uploads/2018/08/chicken-and-rice-in-instant-pot-1015.jpg"
   },
   {
-    "name" : "meal4",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Al Pastor Tacos",
+    "ingredients" : ['Al Pastor', 'Corn Tortillas', 'Onion', 'Cilantro', 'Salsa Verde', 'Salsa Habanero'],
+    "img_url" : "https://iamafoodblog.b-cdn.net/wp-content/uploads/2021/05/al-pastor-3507w.jpg"
   },
   {
-    "name" : "meal5",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Honey Chicken with Rice and Vegetables",
+    "ingredients" : ['Chicken', 'Rice', 'Stir-fry Vegetables', 'Honey', 'Soy Sauce', 'Egg', 'Flour'],
+    "img_url" : "https://i0.wp.com/homemadeonaweeknight.com/wp-content/uploads/2022/05/20220330_183142.jpg"
   },
   {
-    "name" : "meal6",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Ribs with Macaroni Salad and Cornbread",
+    "ingredients" : ['Dry-Seasoned Ribs', 'Tomato', 'Red Onion', 'Macaroni', 'Celery', 'Black Olives', 'Corn Meal', 'Honey', 'Flour', 'Butter'],
+    "img_url" : "https://images.eatsmarter.com/sites/default/files/styles/576x432/public/grilled-ribs-with-cornbread-and-coleslaw-581566.jpg"
   },
   {
-    "name" : "meal7",
-    "ingredients" : [],
-    "img_url" : ""
+    "name" : "Alfredo Zucchini Noodles with Bread",
+    "ingredients" : ['Zucchini', 'Spicy Sausage', 'Onion', 'Garlic', 'Alfredo Sauce', 'Homemade Bread'],
+    "img_url" : "https://cookingwithruthie.com/wp-content/uploads/2015/08/zoodles-with-basil-chicken-alfredo-022-480x480.jpg"
   }
 ]
