679342be192264de51f71887ae36bb037e6446b1
MattPeters13 <mapeters@csumb.edu>
2023-05-08 13:32:49 -0700

    added two function.
    one for api (calories and serving size). Other one was to apply those values to the lists for each meal.

--- meals.py
+++ meals.py
@@ -2,36 +2,50 @@ meals_info = [
   {
     "name" : "Bolognese Spaghetti Squash Pasta with Bread",
     "ingredients" : ['Spaghetti Squash', 'Bolognese Sauce', 'Spicy Sausage', 'Homemade Bread'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://everydaydishes.com/wp-content/uploads/2022/08/macro-friendly-dinner-recipes-spaghetti-squash--scaled.jpg"
   },
   {
     "name" : "Stuffed Bell Peppers",
     "ingredients" : ['Bell Peppers', 'Ground Beef', 'Rice', 'Cheese'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://simple-veganista.com/wp-content/uploads/2019/10/best-vegan-stuffed-peppers-recipe-14.jpg"
   },
   {
     "name" : "Chicken with Rice and Mixed Vegetables",
     "ingredients" : ['Chicken', 'Rice', 'Mixed Veggies', 'Bachan Sauce'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://kristineskitchenblog.com/wp-content/uploads/2018/08/chicken-and-rice-in-instant-pot-1015.jpg"
   },
   {
     "name" : "Al Pastor Tacos",
     "ingredients" : ['Al Pastor', 'Corn Tortillas', 'Onion', 'Cilantro', 'Salsa Verde', 'Salsa Habanero'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://iamafoodblog.b-cdn.net/wp-content/uploads/2021/05/al-pastor-3507w.jpg"
   },
   {
     "name" : "Honey Chicken with Rice and Vegetables",
     "ingredients" : ['Chicken', 'Rice', 'Stir-fry Vegetables', 'Honey', 'Soy Sauce', 'Egg', 'Flour'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://i0.wp.com/homemadeonaweeknight.com/wp-content/uploads/2022/05/20220330_183142.jpg"
   },
   {
     "name" : "Ribs with Macaroni Salad and Cornbread",
     "ingredients" : ['Dry-Seasoned Ribs', 'Tomato', 'Red Onion', 'Macaroni', 'Celery', 'Black Olives', 'Corn Meal', 'Honey', 'Flour', 'Butter'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://images.eatsmarter.com/sites/default/files/styles/576x432/public/grilled-ribs-with-cornbread-and-coleslaw-581566.jpg"
   },
   {
     "name" : "Alfredo Zucchini Noodles with Bread",
     "ingredients" : ['Zucchini', 'Spicy Sausage', 'Onion', 'Garlic', 'Alfredo Sauce', 'Homemade Bread'],
+    "serving_size": [],
+    "calories": [],
     "img_url" : "https://cookingwithruthie.com/wp-content/uploads/2015/08/zoodles-with-basil-chicken-alfredo-022-480x480.jpg"
   }
 ]
